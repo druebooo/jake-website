77cb73a5b0611f7ceee76ae5a115e83284453ffa
Andrew Stewart <andrewstewart349@gmail.com>
2024-10-17 15:36:05 -0400

    Third Comitt

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -185,17 +185,17 @@ export const useSignInAccount = () => {
 
   export const useGetPosts = () => {
     return useInfiniteQuery({
-      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-      queryFn: getInfinitePosts as any,
-      getNextPageParam: (lastPage: any) => {
-        if (!lastPage || lastPage.documents.length === 0) {
-          return null;
-        }
-    
-        const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-        return lastId;
-      },
-      initialPageParam: null, // Set initialPageParam to null
+    queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+    queryFn: getInfinitePosts as any,
+    getNextPageParam: (lastPage: any) => {
+    if (lastPage && lastPage.documents.length === 0) {
+    return null;
+    }
+    const lastId = lastPage.documents[lastPage.documents.length -
+    1].$id;
+    return lastId;
+    },
+    initialPageParam: null,
     });
   }
 
