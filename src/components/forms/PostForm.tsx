627e21b9d3db65cc6af34c15f36cc8b5d7ae9729
Andrew Stewart <andrewstewart349@gmail.com>
2024-10-17 16:06:09 -0400

    Eighth Committ

--- src/components/forms/PostForm.tsx
+++ src/components/forms/PostForm.tsx
@@ -43,9 +43,9 @@ const PostForm = ({ post, action }: PostFormProps) => {
   });
 
   // Query
-  const { mutateAsync: createPost, isLoading: isLoadingCreate } =
+  const { mutateAsync: createPost, isPending: isLoadingCreate } =
     useCreatePost();
-  const { mutateAsync: updatePost, isLoading: isLoadingUpdate } =
+  const { mutateAsync: updatePost, isPending: isLoadingUpdate } =
     useUpdatePost();
 
   // Handler
